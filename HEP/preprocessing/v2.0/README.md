b67bcc95cb319384f31bed2bfedfc25beefaff41
eminserin <eminserin@192.168.2.1>
2018-10-06 23:46:19 +0200

    Relocated files.

---
+++ HEP/preprocessing/v2.0/README.md
@@ -0,0 +1,19 @@
+EEG preprocessing pipeline v2.0 for heartbeat evoked potentials analysis.
+
+Preprocessing steps:
+
+1. Resampling
+2. Bandpass filtering (Different filters on EEG and ECG data)
+3. Epoching
+4. Channel interpolation (if necessary)
+5. Reject epochs with artifact
+6. Reference to average
+7. ICA on EEG data with highpass filter at 1 Hz. 
+8. Copying ICA weights to original EEG data with high pass filter at 0.1 Hz 
+9. Component rejection
+10.Segmentation regarding each condition and R-waves.
+
+The reason why we run ICA on data with highpass filter at 1Hz and copy the 
+ICA weights to the original data is that ICA decomposition is not good on 
+0.1Hz highpass filtered data. Minimum frequency of 0.5Hz is recommended for
+sufficent ICA performance (for detailed information; https://github.com/CSC-UW/csc-eeg-tools/wiki/Filtering-and-ICA). 
---
+++ HEP/preprocessing/v2.0/batchICA.m
@@ -0,0 +1,68 @@
+function [] = batchICA()
+% batchICA runs ICA on multiple pre-ICA EEG data. 
+%
+% Required input structure:
+%   subID_preICA_toICA.set - The dataset that ICA will run on. Should be at
+%       least 0.5Hz highpass filtered.
+%   subID_preICA_org.set - Original dataset which will be used in further
+%       analysis. ICA weights of "toICA" dataset is copied in this file.   
+%
+% Usage: 
+%   batchICA();
+%
+% CAUTION: 
+%   It needs an original date set file in addition to toICA set to copy ICA
+%       weights in it. Please read README.md file. 
+%   
+% Please import multiple files using the pop-up window. Input EEG files
+% must be preprocessed with preICA steps (e.g. resampled,
+% bandpass-filtered, interpolated and averaged).
+%
+% Redesigned for heartbeat evoked potentials analysis. 
+%
+% Emin Serin - Berlin School of Mind and Brain
+%
+
+%% ICA Batch.
+% Load multiple preICA *.set files. 
+[files,eegpath] = uigetfile('*_toICA.set','Please select ".set" files you want to ICA on.'...
+    ,'MultiSelect','on');
+
+binica = 1; % run binica (set 0 if no binary on the computer). 
+
+if ischar(files) 
+    nfile = 1; 
+else
+    nfile = length(files); 
+end
+
+% ICA loop.
+for i = 1: nfile
+    if nfile ~= 1
+        cfile = files{t};
+    else
+        cfile = files;
+    end
+    EEG = pop_loadset(cfile, eegpath); % import current file. 
+    spstr = strsplit(cfile,'_'); 
+    cDataName = [spstr{1},'_ica']; % dataname
+    if binica 
+        EEG = pop_runica(EEG, 'icatype','binica','extended',1,'interupt','on'); % run binica. 
+    else
+        EEG = pop_runica(EEG, 'extended',1,'interupt','on'); % run ica. 
+    end
+    % Import original dataset.
+    TMP = pop_loadset([spstr{1},'_',spstr{2},'_org.set']);
+    
+    % Change ICA weights. 
+    TMP.icawinv = EEG.icawinv;
+    TMP.icasphere = EEG.icasphere;
+    TMP.icaweights = EEG.icaweights;
+    TMP.icachansind = EEG.icachansind;
+    
+    EEG = TMP; % change current dataset to original set. 
+    EEG.setname = cDataName; % change setname with current dataname
+    pop_saveset( EEG, 'filename', EEG.setname, 'filepath', eegpath); % save ICA .set file. 
+end
+
+end
